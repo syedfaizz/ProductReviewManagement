456a21f714776ec1c2c1d467bcc91613021a835b
syedfaizz <syedfaizz45@gmail.com>
2021-09-06 17:07:31 +0530

    [faisal] Add: uc1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management");
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ ProductID=1,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=2,UserID=1,Rating=4,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=3,UserID=2,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=4,UserID=2,Rating=4,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=5,UserID=3,Rating=3,Review="Good",IsLike=false },
+                new ProductReview(){ ProductID=6,UserID=4,Rating=4,Review="Good",IsLike=false },
+                new ProductReview(){ ProductID=1,UserID=3,Rating=5,Review="Nice",IsLike=true },
+                new ProductReview(){ ProductID=8,UserID=10,Rating=3,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=9,UserID=10,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=10,UserID=10,Rating=2,Review="Bad",IsLike=true },
+                new ProductReview(){ ProductID=11,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=12,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=4,UserID=2,Rating=1.5,Review="Bad",IsLike=false },
+                new ProductReview(){ ProductID=14,UserID=5,Rating=3,Review="nice",IsLike=true },
+                new ProductReview(){ ProductID=15,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=5,UserID=10,Rating=2,Review="Not bad",IsLike=false},
+                new ProductReview(){ ProductID=17,UserID=7,Rating=3.5,Review="Nice",IsLike=true },
+                new ProductReview(){ ProductID=10,UserID=3,Rating=1,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=6,UserID=1,Rating=5,Review="Good",IsLike=true },
+                new ProductReview(){ ProductID=20,UserID=10,Rating=5,Review="Good",IsLike=true },
+            };
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID:-" + list.ProductID + " " + "UserID:-" + list.UserID + " " + "Rating:-" + list.Rating + " " +
+                    "Review:-" + list.Review + " " + "IsLike:-" + list.IsLike);
+            }
+        }
+    }
+}
---
+++ productreview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
